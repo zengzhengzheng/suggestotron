f589024035d5a6d64c6b3d8efe332255cfb3ce23
zengzheng <zengzheng2009@gmail.com>
2016-09-22 09:55:40 +0800

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :topics do
+  root 'topics#index'
+    member do
+      post 'upvote'
+    end
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
